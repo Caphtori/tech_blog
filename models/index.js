d8e6409f202da509cf639640f0a9d84b7d138dfb
Qohelet <benjy.shuter@gmail.com>
2024-01-08 06:28:42 -0500

    Adds association between models.

---
+++ models/index.js
@@ -0,0 +1,32 @@
+const Post = require('./Post.js');
+const User = require('./User.js');
+const Comment = require('./comment.js');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { Post, User, Comment }
