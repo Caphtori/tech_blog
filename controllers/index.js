02c4de33377d6db8fe18effa65292d6a56ee9549
Qohelet <benjy.shuter@gmail.com>
2024-01-08 06:45:22 -0500

    Add the router for the html routes.

--- controllers/index.js
+++ controllers/index.js
@@ -1,2 +1,13 @@
 const router = require('express').Router();
-const { Post, User, Comment } = require('../models');
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes.js');
+const userRoutes = require('./userRoutes.js');
+const postRoutes = require('./postRoutes.js');
+
+router.use('/', homeRoutes);
+router.use('/u', userRoutes);
+router.use('/p', postRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
